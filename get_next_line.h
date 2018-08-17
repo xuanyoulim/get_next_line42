e5ee8a5cb867c09b986781a610634f48cd6b1d5a
xuanyoulim <lyyxy@hotmail.com>
2018-08-17 15:57:00 -0700

    Add base files

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xlim <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/17 15:53:06 by xlim              #+#    #+#             */
+/*   Updated: 2018/08/17 15:55:30 by xlim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+int		get_next_line(const int fd, char **line);
+
+#endif
