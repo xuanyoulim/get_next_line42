11871566527a5c2dfead5ff6f622209c33fa134d
xuanyoulim <lyyxy@hotmail.com>
2018-08-29 23:37:29 -0700

    Update libft

---
+++ libft/srcs/additional-ft/ft_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xlim <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/13 21:34:21 by xlim              #+#    #+#             */
+/*   Updated: 2018/08/28 21:06:34 by xlim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+char		*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*out;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	out = ft_memalloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!out)
+		return (NULL);
+	while (s1[i])
+	{
+		out[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		out[i] = s2[j];
+		i++;
+		j++;
+	}
+	out[i] = '\0';
+	return (out);
+}
